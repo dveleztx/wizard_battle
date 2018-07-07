7cd06b50e9e8a19cf136f46fb072b526fae7743e
dveleztx <dveleztx@gmail.com>
2018-07-07 10:54:52 -0700

    Got the combat system working, but need to reorganize code

--- program.py
+++ program.py
@@ -1,4 +1,7 @@
 # import actors
+import random
+import time
+
 from actors import Wizard, Creature
 
 
@@ -8,38 +11,53 @@ def main():
 
 
 def print_header():
-    print('------------------------')
-    print('    WIZARD GAME APP')
-    print('------------------------')
+    print("------------------------")
+    print("    WIZARD GAME APP")
+    print("------------------------")
     print()
 
 
 def game_loop():
 
+    # Create Creature Objects
     creatures = [
-        Creature('Toad', 1),
-        Creature('Tiger', 12),
-        Creature('Bat', 3),
-        Creature('Dragon', 50),
-        Creature('Evil Wizard', 1000),
+        Creature("Toad", 1),
+        Creature("Tiger", 12),
+        Creature("Bat", 3),
+        Creature("Dragon", 50),
+        Creature("Evil Wizard", 1000),
     ]
 
-    print(creatures)
+    # Create Hero Object
+    hero = Wizard("Certneilius", 75)
 
-    hero = Wizard('Certneilius', 75)
-
+    # Game Logic
     while True:
 
-        cmd = input('Do you [a]ttack, [r]unaway, or [l]ook around? ')
+        active_creature = random.choice(creatures)
+        print("A {} of level {} has appeared from a dark and foggy forest..."
+              .format(active_creature.name, active_creature.level))
+        print()
+
+        cmd = input("Do you [a]ttack, [r]unaway, or [l]ook around? ")
         if cmd == 'a':
-            print('attack')
+            if hero.attack(active_creature):
+                creatures.remove(active_creature)
+            else:
+                print("The wizard runs and hides taking time to recover...")
+                time.sleep(5)
+                print("The wizard returns revitalized!")
         elif cmd == 'r':
-            print('runaway')
+            print("The wizard has become unsure of his power and flees!!!")
         elif cmd == 'l':
-            print('look around')
+            print("THe wizard {} takes in the surroundings and sees: ".format(hero.name))
+            for c in creatures:
+                print(" * A {} of level {}".format(c.name, c.level))
         else:
-            print('OK, exiting game... bye!')
+            print("OK, exiting game... bye!")
             break
+
+        print()
 
 
 if __name__ == '__main__':
